e659ce022cdad578a48f44a566fb6439eebf11df
muzamilsohail04.ui <muzamilsohail04@gmail.com>
2026-08-13 15:55:18 +0500

    first commit

---
+++ conditions.js
@@ -0,0 +1,60 @@
+// console.log("conditions")
+
+// const age = 17;
+
+// if(age >= 18){
+//     console.log("you can caste vote")
+// } else{
+//     console.log("you can not caste vote")
+// }
+
+// const num = -9
+
+// if(num >= 0){
+//     console.log("positive")
+// } else {
+//     console.log("negative")
+// }
+// const num = 0
+// if(num >= 1){
+//     console.log("positive")
+// } else if(num === 0){
+//     console.log("zero")
+// } else {
+//     console.log("negative")
+// }
+
+
+// if(num % 2 ===0){
+//     console.log("even")
+// } else {
+//     console.log("Odd")
+// }
+
+    // let num1 = 5;
+    // let num2 = 7;
+    // let op = "/"
+
+    // if(op === "+"){
+    //     console.log(num1 + num2)
+    // } else if(op === "-"){
+    //     console.log(num1 - num2)
+    // } else if(op === "*"){
+    //     console.log(num1 * num2)
+    // } else if(op === "/"){
+    //     console.log(num1 / num2)
+    // } else {
+    //     console.log("invalid operator")
+    // }
+
+    // const marks = 84;
+    // if(marks >= 90){
+    //     console.log("Grade A")
+    // } else if(marks >= 80){
+    //     console.log("Grade B")
+    // } else if(marks >= 70){
+    //     console.log("Grade C")
+    // } else if(marks >= 60){
+    //     console.log("Grade D")
+    //
+    
